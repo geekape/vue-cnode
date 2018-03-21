0e4217107bb383c1783c8e0f6c139a39df5361a5
geekape <861013016@qq.com>
2018-03-21 18:23:02 +0800

    vue cnode iMovie

---
+++ README.md
@@ -0,0 +1,31 @@
+# VueCnodejs
+
+> 一个Vue的**初/中级**练手项目：重构CnodeJS社区
+
+### 功能
+- [x] 首页列表，下划加载
+- [x] 主题详情/评论信息
+- [x] 作者信息
+
+### 使用项目
+```
+1.克隆项目：      git clone https://github.com/geekape/vue-cnode.git
+2.安装nodejs
+3.安装依赖：      npm install
+4.启动服务：      npm run dev
+5.发布代码：      npm run build
+```
+
+### 技术栈：
+---
+[Vue.js](https://cn.vuejs.org/)
+
+[Vue-router](https://router.vuejs.org/zh-cn/)
+
+[Vue-resource](https://etianqq.gitbooks.io/vue2/content/chapter2-vue-resource.html)
+
+[Element](http://element.eleme.io/#/zh-CN/component/installation)
+
+[CnodeJS API](https://cnodejs.org/api)
+
+
---
+++ src/components/cnodeHead.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class='head'>
+        <router-link to='/'>
+            <img src='../assets/cnodejs.svg' title='cnodejs.svg'>
+        </router-link>
+        <el-button type='text' @click='dialogVisible=true'>关于</el-button>
+        <el-dialog title='Vue.js重写CnodeJS社区' v-model='dialogVisible' size='tiny'>
+            <div class='dialogDiv'>
+                <span>
+                    <strong>作者: &nbsp;&nbsp;</strong>
+                </span>
+                <a href='http://www.zcbboke.com' target='_blank'>Geekape</a>
+                </span>
+                <br>
+                <span>
+                    <strong>源代码: &nbsp;&nbsp;</strong>
+                </span>
+                <a href='https://github.com/geekape/vue-cnode' target='_blank'>Github</a>
+                <span class='star'>&nbsp;&nbsp;求Star</span>
+                <br>
+                <br>
+                <span>
+                    <strong>技术栈: </strong>
+                </span>
+                <ul>
+                    <li>Vue.js</li>
+                    <li>Vue-router</li>
+                    <li>Axios</li>
+                    <li>Element</li>
+                    <li>CnodeJS API</li>
+                </ul>
+            </div>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            dialogVisible: false,
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@import url("https://cdn.bootcss.com/element-ui/1.2.8/theme-default/index.css");
+.head {
+    background: #324057;
+    height: 3.6rem;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    img {
+        width: 10rem;
+        height: 3.5rem;
+        margin-left: 10rem;
+    }
+    >button {
+        display: inline-block;
+        float: right;
+        line-height: 3.6rem;
+        margin-right: 10rem;
+        font-weight: bold;
+        font-size: 20px;
+        color: white;
+        letter-spacing: 2px;
+        padding-top: 0.2rem;
+    }
+    .dialogDiv {
+        font-size: 17px;
+    }
+    ul {
+        margin-left: 1rem;
+    }
+    a {
+        text-decoration:none;
+        color: #13CE66;
+    }
+    .star {
+        font-weight: bold;
+        color: #1D8CE0;
+        font-size: 20px;
+    }
+}
+
+</style>
